fb01f9f56b7529b87169a653842fd20da653c47e
DrPing <23166653+DocteurPing@users.noreply.github.com>
2026-02-19 19:24:55 +0700

    Refactor: add this error (#5)
    * feat: add this error for rate_quality_config
    
    * feat: add this error for currenyc file
    
    * feat: add this error for currency_pair

--- crates/domain/src/types/currency_pair.rs
+++ crates/domain/src/types/currency_pair.rs
@@ -1,7 +1,10 @@
+use thiserror::Error;
+
 use crate::types::currency::Currency;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Error)]
 pub enum CurrencyPairError {
+    #[error("Base and quote currencies cannot be the same")]
     CurrencySame,
 }
 
@@ -49,8 +52,11 @@ fn test_currency_pair() {
 fn test_currency_same() {
     let base = Currency::new("EUR").unwrap();
     let quote = Currency::new("EUR").unwrap();
-    let pair = CurrencyPair::new(base, quote);
+    let pair = CurrencyPair::new(base, quote).err().unwrap();
 
-    assert!(pair.is_err());
-    assert_eq!(pair.err().unwrap(), CurrencyPairError::CurrencySame);
+    assert_eq!(pair, CurrencyPairError::CurrencySame);
+    assert_eq!(
+        format!("{}", pair.to_string()),
+        "Base and quote currencies cannot be the same"
+    );
 }
