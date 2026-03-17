f6caeea73b016a00039db41a895cb04508aaded2
drping <drping974@hotmail.com>
2026-03-17 15:30:14 +0700

    doc: update doc for rate_provider

--- crates/infrastructure/src/repositories/rate_provider/mod.rs
+++ crates/infrastructure/src/repositories/rate_provider/mod.rs
@@ -1,4 +1,6 @@
-/// This module contains the implementation of the rate provider repository, which is responsible for fetching exchange rates from an external API and providing them to the application.
+//! This module contains the implementation of the rate provider repository
+
+/// Data transfer objects used by the rate provider repository for representing exchange rate requests and responses.
 pub mod dto;
 
 /// Concrete implementation of the rate provider repository, which fetches exchange rates from the Frankfurter API.
